1fadf5e7f7d1d3268ec2fc702e2baedce6e64cd2
DSync <haroun7@gmail.com>
2015-02-04 22:10:13 +0530

    function evalCost writter

--- Assignment 1/suboptimal/formulation.cpp
+++ Assignment 1/suboptimal/formulation.cpp
@@ -18,8 +18,8 @@ class SeqState {
 public:
 	int length; 					 	//stores the length of the strings of this state.
 	double cost; 						//Functions as energy too.
-	vector< deque<char> > sequences; 	//deque allows for Faster Swaps and stuff.
-	vector< vector<int> > dashPos;
+	//vector< deque<char> > sequences; 	//deque allows for Faster Swaps and stuff. - Not necessary.
+	vector< vector<int> > dashPos;		//Encapsulates the entire state. NEEDS TO BE SORTED.
 
 	//Misc.
 	void print(); //Called by output.
@@ -92,7 +92,51 @@ void SeqProblem::getNBD(SeqState& state , vector<SeqState>& nbd) {
 	return; //TODO
 }
 
-double evalCost(SeqState& state) {
-	return 0.0; //TODO
+double SeqProblem::evalCost(SeqState& state) {
+	double cost = 0.0;
+	/*
+	Following algorithm :
+	forAll 0 <= i < j < k :
+	cost = MisMatchCost( String_i [...ctr] , String_j [...ctr]) where String_i and String_j are the i'th and j'tj resultant strings.
+		It can be calculated easily without copying and inserting and stuff.
+		maintain 5 pointers, two for the dashes , two for the sequences and one for the length processed. Repeat this for every pair.
+	*/
+	for( stringIDX_1 = 0; stringIDX_1 < k; ++stringIDX_1) {
+		for( stringIDX_2 = stringIDX_1+1 ; stringIDX_2 < k; ++stringIDX_2) {
+			int ctr = 0; //Counts how many positions have been parsed.
+			//Pointers to the dash/sequence thingies			
+			int dashIDX_1 	= 0;	
+			int seqIDX_1 	= 0;
+			int dashIDX_2 	= 0;
+			int seqIDX_2	= 0;
+			while ( ctr < state.length ) {
+				//StringIDX_1 's character is calculated here.
+				char s1 , s2; //s1 and s2 are the 
+				if ( state.dashPos[stringIDX_1][dashIDX_1] == ctr ) {
+					//String_IDX1 has a '-' at this position.
+					s1 = '-';
+					cost += CC;
+					++dashIDX_1;
+				} else {
+					s1 = sequences[stringIDX_1][seqIDX_1];
+					++seqIDX_1;
+				}
+
+				//StringIDX_2 's character is calculated here.
+				if ( state.dashPos[stringIDX_2][dashIDX_2] == ctr) {
+					//String_IDX2 has a '-' at this position
+					s2 = '-';
+					cost += CC;
+					++dashIDX_2;
+				} else {
+					s1 = sequences[stringIDX_2][seqIDX_2];
+					++seqIDX_2;
+				}
+				cost += MC[ charToInt[s1] ][ charToInt[s2] ];
+				++ctr;
+			}	
+		}
+	}
+	return cost;
 }
 #endif
