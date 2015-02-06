99b668443915ea1afdc5998122a09cd62c6bfc60
Akshay Kumar Gupta <akshay.akgupta@gmail.com>
2015-02-06 22:44:28 +0530

    Still O(KN) :(

--- Assignment 1/suboptimal/formulation.cpp
+++ Assignment 1/suboptimal/formulation.cpp
@@ -166,7 +166,7 @@ void SeqProblem::getNBD_singleDashMove(const SeqState& state , vector<SeqState>&
 				int tempctr2 = di;
 					//There is one element before di.
 					//ASSERT : child.dashPos[stringIDX][di] > child.dashPos[stringIDX][di-1]
-					while ((tempctr2 > 0) && (child.dashPos[stringIDX][tempctr2] > child.dashPos[stringIDX][tempctr2-1])) {
+					while ((tempctr2 > 0) && (child.dashPos[stringIDX][tempctr2] < child.dashPos[stringIDX][tempctr2-1])) {
 						//Swap to restore invariant.
 						int temp = child.dashPos[stringIDX][tempctr2];
 						child.dashPos[stringIDX][tempctr2] = child.dashPos[stringIDX][tempctr2-1];
@@ -176,9 +176,9 @@ void SeqProblem::getNBD_singleDashMove(const SeqState& state , vector<SeqState>&
 				//std::sort(child.dashPos[stringIDX].begin(), child.dashPos[stringIDX].end()); //Possibly slowing us down.
 				//child.cost = evalCost(child);
 				if(tempctr == di)
-					setChildCost_singleDash(state, child , stringIDX, di, prevValue, tempctr2);
+					setChildCost_singleDash(state, child , stringIDX);//, di, prevValue, tempctr2);
 				else
-					setChildCost_singleDash(state, child , stringIDX, di, prevValue, tempctr);
+					setChildCost_singleDash(state, child , stringIDX);//, di, prevValue, tempctr);
 				nbd.push_back(child);
 			}
 			if (state.dashPos[stringIDX][di] < stringLengths[stringIDX] ) {
@@ -189,31 +189,27 @@ void SeqProblem::getNBD_singleDashMove(const SeqState& state , vector<SeqState>&
 				Feasible alternative to sorting is to assert child.dsahPos[stringIDX][di] > child.dsahPos[stringIDX][di+1] 
 															and also to assert child.dashPos[stringIDX][di] < childdashPos[stringIDX][di-1]
 				*/
-				// if ( di+1 < child.dashPos[stringIDX].size()) {
-// 					//There is another element after di
-// 					//ASSERT : child.dashPos[stringIDX][di] < child.dashPos[stringIDX][di+1]
-// 					if (child.dashPos[stringIDX][di] < child.dashPos[stringIDX][di+1]) {
-// 						//PASS
-// 					} else {
-// 						//Swap to restore invariant.
-// 						int temp = child.dashPos[stringIDX][di];
-// 						child.dashPos[stringIDX][di] = child.dashPos[stringIDX][di+1];
-// 						child.dashPos[stringIDX][di+1] = temp;
-// 					}
-// 				}
-// 				if ( di > 0 ) {
-// 					//There is one element before di.
-// 					//ASSERT : child.dashPos[stringIDX][di] > child.dashPos[stringIDX][di-1]
-// 					if (child.dashPos[stringIDX][di] > child.dashPos[stringIDX][di-1]) {
-// 						//PASS
-// 					} else {
-// 						//Swap to restore invariant.
-// 						int temp = child.dashPos[stringIDX][di];
-// 						child.dashPos[stringIDX][di] = child.dashPos[stringIDX][di-1];
-// 						child.dashPos[stringIDX][di-1] = temp;
-// 					}
-// 				}
-				std::sort(child.dashPos[stringIDX].begin(), child.dashPos[stringIDX].end()); //Possibly slowing us down.
+				int tempctr = di; //loop variable for while loop
+					//There is another element after di
+					//ASSERT : child.dashPos[stringIDX][di] < child.dashPos[stringIDX][di+1]
+				while ((child.dashPos[stringIDX].size() > tempctr + 1) && (child.dashPos[stringIDX][tempctr] > child.dashPos[stringIDX][tempctr + 1])) {
+					//Swap to restore invariant.
+					int temp = child.dashPos[stringIDX][tempctr];
+					child.dashPos[stringIDX][tempctr] = child.dashPos[stringIDX][tempctr + 1];
+					child.dashPos[stringIDX][tempctr + 1] = temp;
+					++tempctr;
+				}
+				int tempctr2 = di;
+					//There is one element before di.
+					//ASSERT : child.dashPos[stringIDX][di] > child.dashPos[stringIDX][di-1]
+					while ((tempctr2 > 0) && (child.dashPos[stringIDX][tempctr2] < child.dashPos[stringIDX][tempctr2-1])) {
+						//Swap to restore invariant.
+						int temp = child.dashPos[stringIDX][tempctr2];
+						child.dashPos[stringIDX][tempctr2] = child.dashPos[stringIDX][tempctr2-1];
+						child.dashPos[stringIDX][tempctr2-1] = temp;
+						tempctr2--;
+					}
+				//std::sort(child.dashPos[stringIDX].begin(), child.dashPos[stringIDX].end()); //Possibly slowing us down.
 				//child.cost = evalCost(child);
 				setChildCost_singleDash(state, child , stringIDX);
 				nbd.push_back(child);
@@ -373,78 +369,86 @@ void SeqProblem::setChildCost_singleDash(const SeqState& parent ,  SeqState& chi
 	child.cost = parent.cost + deltaCost;
 }
 
-
-void SeqProblem::setChildCost_singleDash(const SeqState& parent ,  SeqState& child , int stringIDX_1, int di, int prevValue, int newdi) { //Children are genereated by moving a dash around for one string.
-	
-	double deltaCost = 0.0;
-	for(int stringIDX_2 = 0; stringIDX_2 < k ; ++stringIDX_2) {
-		if ( stringIDX_2 == stringIDX_1 )
-			continue;
-		else {
-			int ctr = 0; //Counts how many positions have been parsed.
-			//Pointers to the dash/sequence thingies			
-			int dashIDX_1 	= 0;	
-			int seqIDX_1 	= 0;
-			int dashIDX_2 	= 0;
-			int seqIDX_2	= 0;
-			while ( ctr < parent.length ) {
-				//StringIDX_1 's character is calculated here.
-				//Parent Cost
-					char s1 , s2; //s1 and s2 are the 
-					if ( (dashIDX_1<parent.dashPos[stringIDX_1].size() ) && (parent.dashPos[stringIDX_1][dashIDX_1] + dashIDX_1 == ctr)) {
-						//String_IDX1 has a '-' at this position.
-						s1 = '-';
-						++dashIDX_1;
-					} else {
-						s1 = sequences[stringIDX_1][seqIDX_1];
-						++seqIDX_1;
-					}
-
-					//StringIDX_2 's character is calculated here.
-					if ( (dashIDX_2 < parent.dashPos[stringIDX_2].size() ) && (parent.dashPos[stringIDX_2][dashIDX_2] + dashIDX_2 == ctr)) {
-						//String_IDX2 has a '-' at this position
-						s2 = '-';
-						++dashIDX_2;
-					} else {
-						s2 = sequences[stringIDX_2][seqIDX_2];
-						++seqIDX_2;
-					}
-					deltaCost -= MC[ charToInt[s1] ][ charToInt[s2] ];
-					ctr++;
-			}
-			ctr=0;
-			dashIDX_1 	= 0;	
-			seqIDX_1 	= 0;
-			dashIDX_2 	= 0;
-			seqIDX_2	= 0;
-			while ( ctr < child.length) {
-				//ChildCost.
-					char c1 , c2;
-					if ( (dashIDX_1< child.dashPos[stringIDX_1].size() ) && (child.dashPos[stringIDX_1][dashIDX_1] + dashIDX_1 == ctr)) {
-						//String_IDX1 has a '-' at this position.
-						c1 = '-';
-						++dashIDX_1;
-					} else {
-						c1 = sequences[stringIDX_1][seqIDX_1];
-						++seqIDX_1;
-					}
-
-					//StringIDX_2 's character is calculated here.
-					if ( (dashIDX_2<child.dashPos[stringIDX_2].size()) && (child.dashPos[stringIDX_2][dashIDX_2] + dashIDX_2 == ctr)) {
-						//String_IDX2 has a '-' at this position
-						c2 = '-';
-						++dashIDX_2;
-					} else {
-						c2 = sequences[stringIDX_2][seqIDX_2];
-						++seqIDX_2;
-					}
-					deltaCost += MC[ charToInt[c1] ][ charToInt[c2] ];
-				++ctr;
-			}	
-		}
-	}
-	child.cost = parent.cost + deltaCost;
-}
+//
+// void SeqProblem::setChildCost_singleDash(const SeqState& parent ,  SeqState& child , int stringIDX_1, int di, int prevValue, int newdi) { //Children are genereated by moving a dash around for one string.
+//
+// 	double deltaCost = 0.0;
+// 	for(int stringIDX_2 = 0; stringIDX_2 < k ; ++stringIDX_2) {
+// 		if ( stringIDX_2 == stringIDX_1 )
+// 			continue;
+// 		else {
+//
+// 			//subtract previous cost
+// 			char s1 = '-';
+// 			char s2 = sequences[stringIDX_2][prevValue + di];
+// 			deltaCost -= MC[ charToInt[s1] ][ charToInt[s2] ];
+// 			s1 = sequences[stringIDX_1][parent.dashPos[stringIDX_1][newdi]];
+// 			s2 = sequences[stringIDX_2][parent.dashPos[stringIDX_2][newdi]];
+// 			int ctr = 0; //Counts how many positions have been parsed.
+// 			//Pointers to the dash/sequence thingies
+// 			int dashIDX_1 	= 0;
+// 			int seqIDX_1 	= 0;
+// 			int dashIDX_2 	= 0;
+// 			int seqIDX_2	= 0;
+//
+// 			while ( ctr < parent.length ) {
+// 				//StringIDX_1 's character is calculated here.
+// 				//Parent Cost
+// 					char s1 , s2; //s1 and s2 are the
+// 					if ( (dashIDX_1<parent.dashPos[stringIDX_1].size() ) && (parent.dashPos[stringIDX_1][dashIDX_1] + dashIDX_1 == ctr)) {
+// 						//String_IDX1 has a '-' at this position.
+// 						s1 = '-';
+// 						++dashIDX_1;
+// 					} else {
+// 						s1 = sequences[stringIDX_1][seqIDX_1];
+// 						++seqIDX_1;
+// 					}
+//
+// 					//StringIDX_2 's character is calculated here.
+// 					if ( (dashIDX_2 < parent.dashPos[stringIDX_2].size() ) && (parent.dashPos[stringIDX_2][dashIDX_2] + dashIDX_2 == ctr)) {
+// 						//String_IDX2 has a '-' at this position
+// 						s2 = '-';
+// 						++dashIDX_2;
+// 					} else {
+// 						s2 = sequences[stringIDX_2][seqIDX_2];
+// 						++seqIDX_2;
+// 					}
+// 					deltaCost -= MC[ charToInt[s1] ][ charToInt[s2] ];
+// 					ctr++;
+// 			}
+// 			ctr=0;
+// 			dashIDX_1 	= 0;
+// 			seqIDX_1 	= 0;
+// 			dashIDX_2 	= 0;
+// 			seqIDX_2	= 0;
+// 			while ( ctr < child.length) {
+// 				//ChildCost.
+// 					char c1 , c2;
+// 					if ( (dashIDX_1< child.dashPos[stringIDX_1].size() ) && (child.dashPos[stringIDX_1][dashIDX_1] + dashIDX_1 == ctr)) {
+// 						//String_IDX1 has a '-' at this position.
+// 						c1 = '-';
+// 						++dashIDX_1;
+// 					} else {
+// 						c1 = sequences[stringIDX_1][seqIDX_1];
+// 						++seqIDX_1;
+// 					}
+//
+// 					//StringIDX_2 's character is calculated here.
+// 					if ( (dashIDX_2<child.dashPos[stringIDX_2].size()) && (child.dashPos[stringIDX_2][dashIDX_2] + dashIDX_2 == ctr)) {
+// 						//String_IDX2 has a '-' at this position
+// 						c2 = '-';
+// 						++dashIDX_2;
+// 					} else {
+// 						c2 = sequences[stringIDX_2][seqIDX_2];
+// 						++seqIDX_2;
+// 					}
+// 					deltaCost += MC[ charToInt[c1] ][ charToInt[c2] ];
+// 				++ctr;
+// 			}
+// 		}
+// 	}
+// 	child.cost = parent.cost + deltaCost;
+// }
 
 
 #define FOLLOW_SCHEDULE_THRESHOLD 0.05
