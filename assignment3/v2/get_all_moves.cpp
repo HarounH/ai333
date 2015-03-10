080c73ead7fee0061500e394d80a6a52ee8655a1
DSync <haroun7@gmail.com>
2015-03-10 08:25:06 +0530

    synchronizing H and git. TODO : Fixup

--- assignment3/v2/get_all_moves.cpp
+++ assignment3/v2/get_all_moves.cpp
@@ -10,19 +10,31 @@ void State::get_all_moves(vector<Move>& moves) { //Assumes that the vector has b
 	if ( present_player == 1 ) {
 		mov._pr = player_1.first;
 		mov._pc = player_1.second;
-		int nr,nc;
+		int nr,nc; // the 'n' prefix is for "No_reason" - nerdy/geeky jokes hurrah!
 			nr = player_1.first;
 			nc = player_1.second;
 		//PART 1 ::::::::::: GET JUMPY MOVES.
-		// y+1 Move.
+		// c+1 Move.
 			//No other player at given spot, and no wall between me and that new spot.
-		if 
-		// y-1 Move.
-		// x+1 Move.
-		// x-1 Move.
+		if ( nc+1 < M ) {
+			if ( (player_2.first == nr) && (player_2.second == nc+1) ) { //Player blocks
+				//Try to jump.
+				//Try to jump to nc+2, if thats possible, then great! , else, jump left and right if those are blocked, try nr-1 and nr+1 
+			} else if ( !wall_V[][] ) { //Wall blocks.
+				//Do Nothing about this.
+			} else { //Player doesnt block, wall doesn't block, 
+				mov.r = nr;
+				mov.c = nc+1
+				mov.m=0;
+				moves.push_back(mov);
+			}
+		}
+		// c-1 Move.
+		// r+1 Move.
+		// r-1 Move.
 		
 		//PART 2 :::::::::: PLACE WALLS.
-		if ( n_player_1_walls > 0 ) { //Check if s/he CAN place walls.
+		if ( n_player_1_walls > 0 ) { //Check if he CAN place walls.
 			for( int _r = 2 ; _r <= N ; ++_r) {
 				for( int _c = 2; _c <= M ; ++ _c ) {
 					//Horizontal Wall.
