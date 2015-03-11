bc6fcb8c4373ec1d153d42e1035d15458eb2ab4d
DSync <haroun7@gmail.com>
2015-03-11 08:48:15 +0530

    compiling, efficient move generator. buggy valid_jump(Move&) function.

--- assignment3/v3/move_generator.cpp
+++ assignment3/v3/move_generator.cpp
@@ -12,6 +12,115 @@ void State::get_all_moves(std::vector<Move>& moves) {
 	//Part1 : 12 Moves.
 	mov.m = 0; //Jumps!
 	bool flag = true; // This flag tells you if the move related to a double jump has been taken care of.
+	
+
+	//Part1 : Upward Jump.
+	mov.r = pos_present_player.r-1;
+	mov.c = pos_present_player.c;
+	if ( valid_jump(mov) ) {
+		moves.push_back(mov);
+	} else {
+		//double jump.
+		mov.r -= 1;
+		if ( flag && valid_jump(mov) ) {
+			moves.push_back(mov);
+			flag = false;
+		} else if ( flag ) {
+			mov.r += 1;
+
+			mov.c += 1;
+			if ( valid_jump(mov) ) {
+				moves.push_back(mov);
+				flag = false;
+			} 
+			mov.c -= 2;
+			if ( valid_jump(mov) ) {
+				moves.push_back(mov);
+				flag = false;
+			}
+		}
+	}
+
+	//Part2 : Downward
+	mov.r = pos_present_player.r+1;
+	mov.c = pos_present_player.c;
+	if ( valid_jump(mov) ) {
+		moves.push_back(mov);
+	} else {
+		//double jump.
+		mov.r += 1;
+		if ( flag && valid_jump(mov) ) {
+			moves.push_back(mov);
+			flag = false;
+		} else if ( flag ) {
+			mov.r -= 1; // set it back.
+
+			mov.c += 1;
+			if ( valid_jump(mov) ) {
+				moves.push_back(mov);
+				flag = false;
+			} 
+			mov.c -= 2;
+			if ( valid_jump(mov) ) {
+				moves.push_back(mov);
+				flag = false;
+			}
+		}
+	}	
+
+	//Part3 : Rightward.
+	mov.r = pos_present_player.r;
+	mov.c = pos_present_player.c + 1;
+	if ( valid_jump(mov) ) {
+		moves.push_back(mov);
+	} else {
+		mov.c += 1; //double right
+		if ( flag && valid_jump(mov) ) {
+			moves.push_back(mov);
+			flag = false;
+		} else if ( flag ) {
+			mov.c -= 1;
+
+			mov.r+= 1;
+			if ( flag && valid_jump(mov) ) {
+				moves.push_back(mov);
+				flag = false;
+			}
+			mov.r -= 2;
+			if ( flag && valid_jump(mov) ) {
+				moves.push_back(mov);
+				flag = false;
+			}
+		}
+	}
+
+	//Part4 : Leftward.
+	mov.r = pos_present_player.r;
+	mov.c = pos_present_player.c - 1;
+	if ( valid_jump(mov) ) {
+		moves.push_back(mov);
+	} else {
+		mov.c -= 1; //double left
+		if ( flag && valid_jump(mov) ) {
+			moves.push_back(mov);
+			flag = false;
+		} else if ( flag ) {
+			mov.c += 1;
+
+			mov.r+= 1;
+			if ( flag && valid_jump(mov) ) {
+				moves.push_back(mov);
+				flag = false;
+			}
+			mov.r -= 2;
+			if ( flag && valid_jump(mov) ) {
+				moves.push_back(mov);
+				flag = false;
+			}
+		}
+	}
+
+/*
 	//CASE1 : Move UP.
 	mov.r = pos_present_player.r - 1;
 	mov.c = pos_present_player.c    ;
@@ -65,32 +174,28 @@ void State::get_all_moves(std::vector<Move>& moves) {
 	
 	mov.r = pos_present_player.r-1;
 	mov.c = pos_present_player.c+1;
-	if ( flag && valid_jump(mov) ) {
+	if ( valid_jump(mov) ) {
 		moves.push_back(mov);
-		flag = false;
 	}
 	//CASE6 : UP&LEFT
 	mov.r = pos_present_player.r-1;
 	mov.c = pos_present_player.c-1;
-	if ( flag && valid_jump(mov) ) {
+	if ( valid_jump(mov) ) {
 		moves.push_back(mov);
-		flag = false;
 	}
 	//CASE7 : DOWN&RIGHT
 	mov.r = pos_present_player.r+1;
 	mov.c = pos_present_player.c+1;
-	if ( flag && valid_jump(mov) ) {
+	if ( valid_jump(mov) ) {
 		moves.push_back(mov);
-		flag = false;
 	}
 	//CASE8 : DOWN&LEFT
 	mov.r = pos_present_player.r+1;
 	mov.c = pos_present_player.c-1;
-	if ( flag && valid_jump(mov) ) {
+	if ( valid_jump(mov) ) {
 		moves.push_back(mov);
-		flag = false;
 	}
-
+*/
 	//part2 : Wall moves.
 	if ( n_present_player_walls>0 ) {
 		for( int _r = 2 ; _r <= N ; ++_r) {
