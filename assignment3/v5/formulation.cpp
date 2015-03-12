dd3ba7404ea476a1ec9c402efcbc26842461b16b
DSync <haroun7@gmail.com>
2015-03-12 17:36:51 +0530

    push!

---
+++ assignment3/v5/formulation.cpp
@@ -0,0 +1,28 @@
+#ifndef FORMULATION_CPP
+#define FORMULATION_CPP
+
+
+#define S_PRINT true
+#define M_PRINT true
+#define P_PRINT true
+#include "formulation.h"
+
+//BFS to check if both players
+#include "bfs.cpp"
+
+//Comms between Client and Player.
+#include "communications.cpp"
+//Initializations and the sort.
+#include "misc.cpp"
+//Checking for connectivity between Positions.
+#include "connectivity.cpp"
+//Generating all the moves - and applying them.
+#include "move_gen.cpp"
+
+
+
+
+//Minimax and eval
+#include "minimax.cpp"
+#include "eval.cpp"
+#endif
