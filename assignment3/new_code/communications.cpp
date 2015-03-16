a3b5c8ccba74547dd2153af35d2bf2fa53295096
artificial-assassin <ee1130504@ee.iitd.ac.in>
2015-03-16 17:03:22 +0530

    muahaha

--- assignment3/new_code/communications.cpp
+++ assignment3/new_code/communications.cpp
@@ -59,10 +59,17 @@ void Player::send_move_to_client_cpp(int& m , int& r , int& c) {
 
 		snair_flag = true;		// making it false, last hard coded move
 	} //Safely disregarding time for hard coded moves.
- 	if (snair_flag && shouldimmx) {
+ 	
+	if (snair_flag && shouldimmx and plies<50) {
  		cout << "yo" << endl;
  		res = ordinary_mmx(2,10); /* H - Need to make changes here. */
  	}
+	
+	if (snair_flag && shouldimmx and plies>=50) {
+ 		cout << "muahahaha" << endl;
+ 		res = ordinary_mmx(4,10); /* H - Need to make changes here. */
+ 	}
+	
 
 		res.m = best_move.m;
 		res.r = best_move.to.r;
